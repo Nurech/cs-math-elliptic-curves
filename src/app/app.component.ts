3380ce8b0a1fece6562cb3a5a276de14161fa530
Joosep Parts <joosep.parts@gmail.com>
2022-10-04 20:33:45 +0300

    bootstrap, prime, echarts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as echarts from 'echarts';
+import { EChartsOption } from 'echarts/types/dist/echarts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cs-math-elliptic-curves';
+export class AppComponent implements AfterViewInit{
+  @ViewChild('chart') chart!: ElementRef;
+  chartOption = option;
+  a = -7;
+  b = 10;
+
+  ngAfterViewInit() {
+    this.setOptions();
+  }
+
+  setOptions() {
+    this.chartOption = option;
+  }
 }
+
+export const option: EChartsOption = {
+  animation: false,
+  grid: {
+    top: 40,
+    left: 50,
+    right: 40,
+    bottom: 50
+  },
+  xAxis: {
+    name: 'x',
+    minorTick: {
+      show: true
+    },
+    minorSplitLine: {
+      show: true
+    }
+  },
+  yAxis: {
+    name: 'y',
+    min: -100,
+    max: 100,
+    minorTick: {
+      show: true
+    },
+    minorSplitLine: {
+      show: true
+    }
+  },
+  dataZoom: [
+    {
+      show: true,
+      type: 'inside',
+      filterMode: 'none',
+      xAxisIndex: [0],
+      startValue: -10,
+      endValue: 10
+    },
+    {
+      show: true,
+      type: 'inside',
+      filterMode: 'none',
+      yAxisIndex: [0],
+      startValue: -10,
+      endValue: 10
+    }
+  ],
+  series: [
+    {
+      type: 'line',
+      smooth: true,
+      showSymbol: false,
+      clip: true,
+      data: [[-1,-1],[1,1], [1,3]]
+    }
+  ]
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,16 @@ import { StoreModule } from '@ngrx/store';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')
+    }),
+    InputNumberModule,
+    FormsModule,
+    NgbModule,
+    ButtonModule,
+    RippleModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
