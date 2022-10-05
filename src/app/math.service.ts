098cfc5f0477b153baa8cb0dba977dd97e4b34db
Joosep Parts <joosep.parts@gmail.com>
2022-10-05 06:51:35 +0300

    commit

--- src/app/math.service.ts
+++ src/app/math.service.ts
@@ -6,4 +6,16 @@ import { Injectable } from '@angular/core';
 export class MathService {
 
   constructor() { }
+
+  /**
+   * y2=x3+ax+b
+   */
+  curve() {
+    let a = 0;
+    let b = 7;
+    let arr = []
+
+    console.warn('arr', arr)
+    return arr;
+  }
 }
