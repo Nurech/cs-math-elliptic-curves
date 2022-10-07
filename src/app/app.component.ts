454c6a5c05de5cc6f928730a0f1ee8ef8f03da84
Joosep Parts <joosep.parts@gmail.com>
2022-10-07 21:35:47 +0300

    math service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import functionPlot from 'function-plot';
+import { FunctionPlotOptions } from 'function-plot/dist/types';
+import { MathService } from './math.service';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +11,18 @@ import functionPlot from 'function-plot';
 export class AppComponent implements AfterViewInit {
 
   @ViewChild('container') container!: ElementRef;
+  private instance: any;
 
-  constructor() {
+  constructor(private math: MathService) {
   }
 
   content = '$y^{2}=x^{3}+ax+b \\mod{N}$';
   a = 0;
   b = 7;
   N = 34;
-  x1 = 2; // P
-  y1 = 2; // P
-  x2 = 4; // Q
+  x1 = -1.39389; // P
+  y1 = 2.07166; // P
+  x2 = 2.08008; // Q
   y2 = 4; // Q
 
 
@@ -28,7 +31,7 @@ export class AppComponent implements AfterViewInit {
   }
 
   build() {
-    let option = {
+    let option: FunctionPlotOptions = {
       target: '#graph',
       width: 450,
       disableZoom: false,
@@ -36,27 +39,33 @@ export class AppComponent implements AfterViewInit {
       xAxis: {domain: [-10, 10]},
       grid: true,
       data: [
-        {
-          fn: 'y^2-((x^3)+(' + this.a + 'x)+' + this.b + ')',
-          color: '#e7a649',
-          fnType: 'implicit',
-        },
-        {fn: 'x', color: 'pink'}
+        {fn: this.math.fnPQ(this.x1, this.y1, this.x2, this.y2), fnType: 'linear'},
+        {fn: '-' + this.math.fnCurve(this.a, this.b), color: '#e7a649'},
+        {fn: this.math.fnCurve(this.a, this.b), color: '#e7a649'},
+        {points: [[1, 1], [2, 1]], color: '#5a528d', fnType: 'points', graphType: 'scatter'}
       ]
     };
-    // @ts-ignore
-    functionPlot(option);
-    console.group("Point addition");
-    console.log('curve: ', JSON.stringify(option.data[0].fn));
-    console.log('line Q-P: ', JSON.stringify(option.data[1].fn));
-    console.groupEnd();
+    this.instance = functionPlot(option);
+    console.log(this.instance);
+    this.log(option);
+  }
 
+
+  // Just logging to console for debug
+  log(option: FunctionPlotOptions) {
+    console.group('Logging...point addition');
+    console.log('Chart option', {option});
+    option?.data?.forEach((d, index) => {
+      console.log('Function [' + index + '] on current graph!: ' + JSON.stringify(d?.fn || d?.points));
+    });
+    console.groupEnd();
   }
 
   refresh() {
     this.build();
   }
 
+  // Reset to defaults
   reset() {
     this.a = 0;
     this.b = 7;
--- src/app/math.service.ts
+++ src/app/math.service.ts
@@ -5,17 +5,32 @@ import { Injectable } from '@angular/core';
 })
 export class MathService {
 
-  constructor() { }
+  /**
+   * Build straight line function for P -> Q
+   * Find the slope m (y1 - y2) / (x1 - x2)
+   * Simplify to slope-intercept y = mx + b
+   */
+  fnPQ(x1: number, y1: number, x2: number, y2: number) {
+    let m = (y2 - y1) / (x2 - x1);
+    let b = y1 - (m * x1);
+    return m + '*x + ' + b;
+  }
 
   /**
-   * y2=x3+ax+b
+   * Weierstrass curve y²=x³+ax+b
+   * To get rid of pow we sqrt right side
    */
-  curve() {
-    let a = 0;
-    let b = 7;
-    let arr = []
-
-    console.warn('arr', arr)
-    return arr;
+  fnCurve(a: number, b: number) {
+    return 'sqrt(x^3+' + a + 'x+' + b + ')';
   }
+
+
+  /**
+   * Find R from P -> Q on the curve, addition law
+   * P=(x1,y1), Q=(y1,y2), R=(x,y3)
+   */
+  fnR() {
+
+  }
+
 }
